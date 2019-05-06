8a0ac4afffc8d7ffe2935f588573ec07ad680ea8
felipe-houpa <felipe.houpa@gmail.com>
2019-05-06 00:14:11 -0300

    database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,14 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
+  host: '192.168.1.101',
   username: 'docker',
   port: 5432,
   password: 'docker',
   database: 'sequelize_tests',
   operatorAliases: false,
   define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true
+    timestamps: false
+    // underscored: true,
+    // underscoredAll: true
   }
 }
